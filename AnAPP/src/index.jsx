186f4b729f813e112ddd76ff8ab84947e7b06bbc
Artur <a.dylewski@student.utwente.nl>
2024-03-05 11:11:40 +0100

    List of apps

--- AnAPP/src/index.jsx
+++ AnAPP/src/index.jsx
@@ -14,19 +14,22 @@ const userID = "1001"
     const App = () => {
         const [username, setUsername] = useState('');
         const [products, setProducts] = useState([]);
-
+        const [showText, setShowText] = useState(true);
+        
+        
+        let variable = true
         async function getRequester() {
             const response = await api.fetch('https://bartgeugies.com/', {
             method: 'GET',
         });
-        
+            
             await setUsername("Bart");
             console.log( await response.json());
 
             const boxes = [<Checkbox value="confluence" label="conclufecnce" />];
         
             setProducts(boxes);
-
+            setShowText(!showText);
         
             console.log(products)
         
@@ -53,10 +56,18 @@ const userID = "1001"
             <Fragment>
                 <Text>This is my app! i have no idea if this works</Text>
                 <Button text="CLICK" onClick={getRequester}/>
+                <Button text="CLICK" onClick={variable = !variable}/>
                 <Text>Hello this is a text</Text>
+                {showText && <Text>Hello this is a text</Text>}
                 <Form onSubmit={handleFormSubmit}>
                     <TextField id="hello" name="username" label={username} value={username} onChange={setUsername} />
                     <CheckboxGroup name="products" label="Products" children={products} onChange={setProducts} >
+                        {variable && <Checkbox value="confluence" label="Stack" />}
+                        {showText && <Checkbox value="confluence" label="Hipo" />}
+                        {showText && <Checkbox value="confluence" label="Asset management" />}        
+                        {showText && <Checkbox value="confluence" label="BOS" />}
+                        {showText && <Checkbox value="confluence" label="Vast" />}
+                        {showText && <Checkbox value="confluence" label="CustomerPortal" />}
                         
                     </CheckboxGroup>
                 </Form>
