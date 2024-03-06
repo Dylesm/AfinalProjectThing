7056b013d94b5cc239b9689c244f44b5828242f9
Artur <a.dylewski@student.utwente.nl>
2024-03-06 10:38:55 +0100

    I dunno what this is

--- AnAPP/src/index.jsx
+++ AnAPP/src/index.jsx
@@ -1,5 +1,5 @@
 import ForgeUI, {render,useEffect, Fragment, Text, Button, PortalSubheader, Form, TextField, Checkbox, CheckboxGroup, useState} from '@forge/ui';
-import api from '@forge/api';
+import api,{ route } from '@forge/api';
 const userID = 1001;
     
 
@@ -8,7 +8,6 @@ const userID = 1001;
     
        
 
-    
     
 
     const App = () => {
@@ -25,19 +24,198 @@ const userID = 1001;
         const [products, setProducts] = useState([]);
         const [showText, setShowText] = useState(true);
 
-        const test = ['test1', 'test2']
+    
         
         
         
         
 
+
+
+        
         let variable = true
+        async function createIssue(){
+
+   
+        }
+
+
+
+// This sample uses Atlassian Forge
+// https://developer.atlassian.com/platform/forge/
+
+
+var bodyData = `{
+  "fields": {
+    "assignee": {
+      "id": "5b109f2e9729b51b54dc274d"
+    },
+    "components": [
+      {
+        "id": "10000"
+      }
+    ],
+    "customfield_10000": "09/Jun/19",
+    "customfield_20000": "06/Jul/19 3:25 PM",
+    "customfield_30000": [
+      "10000",
+      "10002"
+    ],
+    "customfield_40000": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "Occurs on all orders",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "customfield_50000": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "Could impact day-to-day work.",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "customfield_60000": "jira-software-users",
+    "customfield_70000": [
+      "jira-administrators",
+      "jira-software-users"
+    ],
+    "customfield_80000": {
+      "value": "red"
+    },
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "Order entry fails when selecting supplier.",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "duedate": "2019-05-11",
+    "environment": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "UAT",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "fixVersions": [
+      {
+        "id": "10001"
+      }
+    ],
+    "issuetype": {
+      "id": "10000"
+    },
+    "labels": [
+      "bugfix",
+      "blitz_test"
+    ],
+    "parent": {
+      "key": "PROJ-123"
+    },
+    "priority": {
+      "id": "20000"
+    },
+    "project": {
+      "id": "10000"
+    },
+    "reporter": {
+      "id": "5b10a2844c20165700ede21g"
+    },
+    "security": {
+      "id": "10000"
+    },
+    "summary": "Main order flow broken",
+    "timetracking": {
+      "originalEstimate": "10",
+      "remainingEstimate": "5"
+    },
+    "versions": [
+      {
+        "id": "10000"
+      }
+    ]
+  },
+  "update": {}
+}`;
+
+async function hello(){
+const response = await api.asUser().requestJira(route`/rest/api/3/issue`, {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: bodyData
+});
+
+console.log(`Response: ${response.status} ${response.statusText}`);
+console.log(await response.json());
+
+}
+
+
+        //     const response = await api.fetch('https://bitinc.atlassian.net/rest/api/3/issue', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Authorization': `Basic ${Buffer.from(
+        //           'bitinc2024@gmail.com:ATATT3xFfGF0McuJY1uJEX1woSYKY-R_eL9Cpw4x8SbqTqWIs-j0LgyPMnlvdJlkudQwX7Kfwr3ua40fTayWVHoTw1BAEi7bpheEBIgwh2bUa-mymJwT9dfS0VQZ-9lc8Cq6Xidgzkgpx0l-5B4e7RK_tHM7RAcuPkdOQluOEeDf2EbJW4jsy6w=39938EE4'
+        //         ).toString('base64')}`,
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/json'
+        //       },
+        //     body: {"fields": {
+        //               "assignee": {
+        //                 "id": "5b109f2e9729b51b54dc274d"
+        //               }
+        //     }},
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+        // });
+        //     let data = await response.json();
+        //     console.log(data);
+        // }
+
+        
         async function getRequester() {
             const response = await api.fetch('https://bartgeugies.com/', {
             method: 'GET',
         });
             let data = await response.json();
-            console.log(data[userID]["access"]);
+            //console.log(data[userID]["access"]);
             for (let i of data[userID]["access"]){
                 switch(i){
                     case "Stack":
@@ -60,20 +238,10 @@ const userID = 1001;
                         break;
                 }
                 
-            }
-            
-
-        
-            setShowText(!showText);
-        
-            
-        
+                    
 
         }
-        
-        async function testFunc() {
-            return test[0]
-        }
+    }
         
         const formData = {
             username: '',
@@ -84,16 +252,16 @@ const userID = 1001;
 
         useEffect(() => {
             getRequester();
-            console.log("this is a test");
+           // console.log("this is a test");
         }, []);
 
         const handleFormSubmit = (formData) => {
-            console.log(formData); // This will log the form data when the form is submitted
+           // console.log(formData); // This will log the form data when the form is submitted
         };
         return (
             <Fragment>
                 <Text>This is my app! i have no idea if this works</Text>
-                <Button text="CLICK" onClick={getRequester}/>
+                <Button text="CLICK" onClick={hello}/>
                 
                 <Text>Hello this is a text</Text>
                 {showText && <Text>Hello this is a text</Text>}
