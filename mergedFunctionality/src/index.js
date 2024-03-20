5e52e448ec2015031692548d914c3326492ac189
Artur <a.dylewski@student.utwente.nl>
2024-03-20 16:57:56 +0100

    WORKING stuff

--- mergedFunctionality/src/index.js
+++ mergedFunctionality/src/index.js
@@ -1,11 +1,48 @@
 import Resolver from '@forge/resolver';
+const fetch = require('node-fetch');
 
+import api, { route } from "@forge/api";
 const resolver = new Resolver();
+resolver.define('UpdatePlease', async (req) => {
+  
+  
+    console.log("this is a test");
 
-resolver.define('getText', (req) => {
-  console.log(req);
+    var bodyData = `{
+      "fields": {
+        "customfield_10055":"my new description"
+      }
+    }`;
 
-  return 'Hello, world!';
+    await fetch('https://bitinc.atlassian.net/rest/api/2/issue/BIT-51', {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Basic ${Buffer.from(
+        'bitinc2024@gmail.com:ATATT3xFfGF0McuJY1uJEX1woSYKY-R_eL9Cpw4x8SbqTqWIs-j0LgyPMnlvdJlkudQwX7Kfwr3ua40fTayWVHoTw1BAEi7bpheEBIgwh2bUa-mymJwT9dfS0VQZ-9lc8Cq6Xidgzkgpx0l-5B4e7RK_tHM7RAcuPkdOQluOEeDf2EbJW4jsy6w=39938EE4'
+      ).toString('base64')}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: bodyData
+  })
+    .then(async response => {
+      await console.log(
+        `Response: ${response.status} ${response.statusText}`
+      );
+      return response.text();
+    })
+    .then(async text => await console.log(text))
+    .catch(async err => await console.error(err));
+  
+  return "done";
+  
+
+
+  
+    
 });
+
+
+
 
 export const handler = resolver.getDefinitions();
--- mergedFunctionality/static/portalRD/src/App.js
+++ mergedFunctionality/static/portalRD/src/App.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { invoke } from '@forge/bridge';
+import { invoke, view } from '@forge/bridge';
+import api, { route } from "@forge/api";
 
 function App() {
-  const [data, setData] = useState(null);
+  async function fetchData() {  
+    const context = await view.getContext().then(console.log);
+    return context;
+  }
 
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
+  // This sample uses Atlassian Forge
+// https://developer.atlassian.com/platform/forge/
+
+async function functionEdit(){
+  invoke ("UpdatePlease",{key: "value"}).then(console.log);
+
+
+}
+
+
+
+
+
 
   return (
     <div>
-      {data ? data : 'Loading...'}
+      <h1>Context</h1>
+      <button onClick={fetchData}>Get Context</button>
+      <button onClick={functionEdit}>Editthe issue </button>
     </div>
   );
 }
